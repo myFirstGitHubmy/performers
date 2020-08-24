f7a08617a43734bc2f15fe511611afb7a582b8e3
Evgen <ev.borodin1998@gmail.com>
2020-08-24 20:25:53 +0500

    create new file
    create veb performer

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function clear(element){
+    let clearInput = document.getElementById("")
+}
+
+function request(){
+    let prog = document.getElementById("programm")
+    let text = document.getElementById("request")
+
+
+    prog.innerHTML = prog.innerHTML + "Запросить " + text.value + "</br>" 
+    text.value = ""
+}
