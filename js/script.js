ea0672f57b49ad10e0ae1c21d384357f1bf3ed45
Evgen <ev.borodin1998@gmail.com>
2020-08-26 23:19:10 +0500

    added ListMaps

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-let string_prog = ''
-let status_OK = false
+var LIST = new Map()
+var SYS_VARIABLE = new Map()
+var status_OK = false
+var SYS_INDEX = 1
+var dinamic_variable
 function clear(element){
     let clearInput = document.getElementById("")
 }
 
+function printMap(map){
+    for(let [key,value] of map){
+        console.log("key: " + key + ",value: "+ value)
+    }
+}
+
 function stat(){
+    dinamic_variable = null
     status_OK = false
     string_prog = ''
-    document.getElementById('assign_button_OK').removeAttribute('data-dismiss')
+    document.getElementById('assign_button_OK').removeAttribute("data-dismiss")
 }
 
 function request(){
@@ -22,22 +32,46 @@ function clear_input(){
     assign_input.value = ""
 }
 
+function setSysVariable(k,v){
+    SYS_VARIABLE.set(k,v)
+}
+
+function setList(index){
+    LIST.set(SYS_INDEX,index)
+}
+
+function incrementSysIndex(){
+        SYS_INDEX++
+}
+
 function assign(){
+    
     var prog = document.getElementById("programm")
     let assign_input = document.getElementById("assign")
+    if (/[A-Za-z]/.test(assign_input.value)){
+        assign_input.classList.add='valid'
+        assign_input.classList.remove='invalid'
+    }else{
+        assign_input.classList.remove='valid'
+        assign_input.classList.add='invalid'
+    }
     let assign_def = document.getElementById("assign_def")
     let assign_butt_OK = document.getElementById('assign_button_OK')
 
-    string_prog += assign_def.innerHTML + assign_input.value
-    clear_input();
+    string_prog += assign_def.innerHTML + assign_input.value +" "
     assign_def.innerHTML = 'Значение '
-    string_prog+= assign_def.innerHTML + assign_input.value 
-    
     if (status_OK) {
-        prog.innerHTML = prog.innerHTML+ string_prog
+        prog.innerHTML += string_prog
         assign_def.innerHTML = 'Присвоить '
-        assign_butt_OK.setAttribute('data-dismiss',"modal")}else{
-            status_OK=true}
+        setSysVariable(dinamic_variable,assign_input.value)
+        setList(2)
+        incrementSysIndex()
+        assign_butt_OK.setAttribute("data-dismiss","modal")
+        printMap(SYS_VARIABLE)
+        printMap(LIST)
+    }else{
+        dinamic_variable = assign_input.value
+        status_OK=true}
     clear_input();
-
+    
 }
